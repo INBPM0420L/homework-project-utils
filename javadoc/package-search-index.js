ee59a8f8270b25539dccde1d574f8d9586509bee
jeszy75 <jeszenszky.peter@inf.unideb.hu>
2024-05-02 21:30:33 +0000

    Deploying to javadoc from @ INBPM0420L/homework-project-utils@516c7683991a620de7a365231a5f45899eb1fad7 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"game"},{"l":"puzzle"},{"l":"puzzle.solver"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"game"},{"l":"game.util"},{"l":"puzzle"},{"l":"puzzle.solver"}];updateSearchResults();
