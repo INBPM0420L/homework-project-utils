ede518ccca7b610ca9eab1fb6a4bada387379b2a
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2024-05-17 22:32:10 +0200

    fix: fix equals() violating reflexivity in Node

--- src/main/java/puzzle/solver/Node.java
+++ src/main/java/puzzle/solver/Node.java
@@ -100,7 +100,7 @@ public class Node<T> {
     @Override
     public boolean equals(Object o) {
         if (o == this) {
-            return false;
+            return true;
         }
         return (o instanceof Node other) && state.equals(other.getState());
     }
